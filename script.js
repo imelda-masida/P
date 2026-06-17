5297c31d4d0c74d8e9fa5706baf04f45332e7da6
stone <imeldamasidakakudji@gmail.com>
2026-06-17 23:49:19 +0200

    add img et modification css

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const boutonVideo = document.getElementById('playBtn');
+
+
+boutonVideo.addEventListener('click', function() {
+    
+    
+    alert("Le lecteur vidéo va se lancer !");
+    
+    /* Bonus : Si tu veux rediriger vers une vraie vidéo, 
+    tu pourras remplacer la ligne de l'alert par celle-ci :
+    window.open('https://www.youtube.com/watch?v=REMPLACE_PAR_TON_LIEN', '_blank');
+    */
+});
